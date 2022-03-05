ed20af61ff00dc2b0bddcd2b9df85bc0fb46951e
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-03-05 17:10:22 +0300

    add pie chart

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
 
-const Pie3D = () => {
-  return <div>chart</div>;
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+const Pie3D = ({ data }) => {
+    const chartConfigs = {
+        type: "pie3d",
+        width: "100%",
+        height: 400,
+        dataFormat: "json",
+        dataSource: {
+            chart: {
+                caption: "Languages",
+                decimals: 0,
+                theme: "fusion",
+                pieRadius: "60%",
+            },
+            data,
+        },
+    };
+    return <ReactFC {...chartConfigs} />;
 };
 
 export default Pie3D;
