e2928bb9b49914e8c31b3f02c1bbb6dfe740128e
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-03-05 17:10:54 +0300

    Add doughnutchart

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+const Doughnut2d = ({ data }) => {
+    const chartConfigs = {
+        type: "doughnut2d",
+        width: "100%",
+        height: 400,
+        dataFormat: "json",
+        dataSource: {
+            chart: {
+                caption: "Stars Per Language",
+                decimals: 0,
+                theme: "candy",
+                pieRadius: "60%",
+                showPercentValues: 0,
+            },
+            data,
+        },
+    };
+    return <ReactFC {...chartConfigs} />;
 };
 
 export default Doughnut2d;
