e5f4a25e8707a25af1bc366a7033f8828b1245b1
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-03-07 00:36:27 +0300

    Promise.allSettled( ) for repos and followers

--- src/context/context.js
+++ src/context/context.js
@@ -29,7 +29,26 @@ const GithubProvider = ({ children }) => {
         if (response) {
             setGithubUser(response.data);
             const { login, followers_url } = response.data;
-            // repos
+
+            // repos and followers together
+            await Promise.allSettled([
+                axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+                axios(`${followers_url}?per_page=100`),
+            ])
+                .then((results) => {
+                    const [repos, followers] = results;
+                    const status = "fulfilled";
+                    if (repos.status === status) {
+                        setRepos(repos.value.data);
+                    }
+                    if (followers.status === status) {
+                        setFollowers(followers.value.data);
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+            /*  // repos
             axios(`${rootUrl}/users/${login}/repos?per_page=100`).then(
                 (response) => {
                     setRepos(response.data);
@@ -39,7 +58,7 @@ const GithubProvider = ({ children }) => {
             // followers
             axios(`${followers_url}?per_page=100`).then((response) => {
                 setFollowers(response.data);
-            });
+            }); */
         } else {
             toggleError(true, "there is no user with that username");
         }
